823079f3b670dac309d4bdc9dd7626a05df060cd
aondo <pndoalex2020@gmail.com>
2024-12-06 09:40:12 +0100

    Initial commit

---
+++ app/Http/Controllers/VisiteurController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Visiteur;
+use Exception;
+class VisiteurController extends Controller
+{
+    public function initPasswod(request $request)
+    {
+        try {
+            $hash = b
+
+        }
+    }
+}
