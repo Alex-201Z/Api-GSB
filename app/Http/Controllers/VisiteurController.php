ac9d7802ecc06889271c69d2867872b55604da77
aondo <pndoalex2020@gmail.com>
2024-12-20 09:21:41 +0100

    1.3

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Visiteur;
 use Exception;
+use Illuminate\Support\Facades\Auth;
+
 class VisiteurController extends Controller
 {
     public function initPasswords(request $request)
@@ -17,5 +19,51 @@ class VisiteurController extends Controller
             return response()->json(['status_message' => $e->getMessage()], 500);
         }
 
+    }
+    public function login(Request $request)
+    {   // Vérifie si la reqête est en JSON
+        if ($request->isJson()) {
+            // Validation des données reçues, il faut un login et un password
+            $request->validate([
+                'login' => 'required',
+                'password' => 'required',
+            ]);
+
+            // Authentification login/password
+            $login=$request->json('login');
+            $pwd=$request->json('password');
+            $credentials = ['login_visiteur' => $login, 'password' => $pwd];
+            if (!Auth::attempt($credentials)) {
+                return response()->json(['error' => 'The provided credentials are incorrect.'], status: 401);
+            }
+
+            // On crée un token pour le visiteur authentifié
+            $visiteur = $request->user();
+            $token = $visiteur->createToken('auth_token')->plainTextToken;
+
+            // On retourne un JSON
+            return response()->json([
+                'visiteur' => [
+                    'id_visiteur'      => $visiteur->id_visiteur,
+                    'nom_visiteur'     => $visiteur->nom_visiteur,
+                    'prenom_visiteur'  => $visiteur->prenom_visiteur,
+                    'type_visiteur'    => $visiteur->type_visiteur,
+                ],
+                'access_token' => $token,
+                'token_type' => 'bearer',
+            ]);
+        }
+        // Gestion des erreurs si la requête n'est pas en JSON
+        return response()->json(['error' => 'Request must be JSON.'], status: 415);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function unauthorized()
+    {
+        return response()->json(['error' => 'Unauthorized access']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 //Route::POST('/visiteur/initpwds', [VisiteurController::class,"initPasswords"]);
+Route::get('/visiteur/logout', [VisiteurController::class,"logout"])->middleware('auth:sanctum');
+Route::post('/visiteur/login', [VisiteurController::class,"login"]);
+Route::get('/visiteur/unauth', [VisiteurController::class,"unauthorized"])-> name("login");
+
+Route::get('/frais/{idFrais}', [FraisController::class,"unauthorized"]);
+Route::get('/frais/ajout', [FraisController::class,"unauthorized"]);
+Route::get('/frais/modif', [FraisController::class,"unauthorized"]);
+Route::get('/frais/suppr', [FraisController::class,"unauthorized"]);
+Route::get('/frais/liste/{idVisiteur}', [FraisController::class,"unauthorized"]);
